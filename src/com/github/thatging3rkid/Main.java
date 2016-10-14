2ded4127d2ecb7b4e7731c8a2dd2eb512ec17207
Connor <thatging3rkid@users.noreply.github.com>
2016-10-14 18:20:58 -0400

    os detection and config.ini

--- src/com/github/thatging3rkid/Main.java
+++ src/com/github/thatging3rkid/Main.java
@@ -1,10 +1,23 @@
 package com.github.thatging3rkid;
 
+import com.github.thatging3rkid.ui.Graphical;
+import com.github.thatging3rkid.ui.CommandLine;
+
 public class Main {
 
+	public final static String version = "v0.1";
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		if (args.length <= 0) {
+			@SuppressWarnings("unused") //automatically starts
+			Graphical gui =  new Graphical();
+		} else if (args[0].equals("-c")) {
+			@SuppressWarnings("unused")
+			CommandLine cli = new CommandLine(args);
+		}
+		
+		
 	}
 
 }
