03e70e24aceb87e62aa2788e4408e1ed7e7397c4
Connor <thatging3rkid@users.noreply.github.com>
2016-10-14 13:10:41 -0400

    added SteamAccount class

--- src/com/github/thatging3rkid/SteamAccount.java
+++ src/com/github/thatging3rkid/SteamAccount.java
@@ -4,4 +4,24 @@ import java.io.Serializable;
 
 public class SteamAccount implements Serializable {
 
+	private String username;
+	private String showname;
+	private String password;
+	
+	public SteamAccount(String _username, String _showname, String _password) {
+		this.username = _username;
+		this.showname = _showname;
+		this.password = _password;
+	}
+	
+	public String[] getLoginCombo() {
+		String[] output = new String[2];
+		output[0] = this.username;
+		output[1] = this.password;
+		return output;
+	}
+	
+	public String getShowname() {
+		return this.showname;
+	}
 }
